55408ad610c5a33e568ba0eaaccc1fe22b648811
BabulKumar2022 <bb3583400@gmail.com>
2022-05-27 18:51:48 +0600

    dashboard added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 
+import { Routes, Route, Link } from "react-router-dom";
 import './App.css';
+import About from "./components/About/About";
+import Dashboard from "./components/Dashboard/Dashboard";
+import Grocery from "./components/Grocery/Grocery";
+import Home from './components/Home/Home';
+import Navbar from './components/Shared/Navbar';
 
 function App() {
   return (
     <div className="">
-      <h1 className='text-center text-4xl'>Home</h1>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/grocery" element={<Grocery />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
     </div>
   );
 }
