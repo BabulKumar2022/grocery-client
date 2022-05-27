3754344ef90e99377379c3e6db6cf6401e58b24d
BabulKumar2022 <bb3583400@gmail.com>
2022-05-27 22:29:29 +0600

    dashboard update

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@
 import { Routes, Route, Link } from "react-router-dom";
 import './App.css';
 import About from "./components/About/About";
+import AddProduct from "./components/Dashboard/AddProduct";
 import Dashboard from "./components/Dashboard/Dashboard";
+import ManageProduct from "./components/Dashboard/ManageProduct";
 import Grocery from "./components/Grocery/Grocery";
 import Home from './components/Home/Home';
 import Navbar from './components/Shared/Navbar';
@@ -15,7 +17,10 @@ function App() {
         <Route path="/home" element={<Home />} />
         <Route path="/grocery" element={<Grocery />} />
         <Route path="/about" element={<About />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/dashboard" element={<Dashboard />}>
+          <Route index element={<AddProduct></AddProduct>}></Route>
+          <Route path="manage" element={<ManageProduct></ManageProduct>}></Route>
+         </Route>
       </Routes>
     </div>
   );
--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,24 +1,28 @@
-import React from 'react';
+import React from "react";
+import { Link, Outlet } from "react-router-dom";
 
 const Dashboard = () => {
-    return (
-        <div class="drawer">
-            <input id="my-drawer" type="checkbox" class="drawer-toggle" />
-            <div class="drawer-content">
-                {/* <!-- Page content here --> */}
-                <label for="my-drawer" class="btn btn-primary drawer-button">Open drawer</label>
-                    </div> 
-        <div class="drawer-side">
-            <label for="my-drawer" class="drawer-overlay"></label>
-            <ul class="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
-            {/* <!-- Sidebar content here --> */}
-            <li><a>Sidebar Item 1</a></li>
-            <li><a>Sidebar Item 2</a></li>
-            
-            </ul>
-        </div>
-</div>
-    );
+  return (
+    <div class="drawer drawer-mobile">
+    <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
+    <div class="drawer-content  ">
+      {/* <!-- Page content here --> */}
+      <h2 className="text-4xl text-center my-5 text-blue-400">Dashboard</h2>
+        <Outlet></Outlet>
+      <label for="my-drawer-2" class="btn btn-primary drawer-button lg:hidden">Open drawer</label>
+    
+    </div> 
+    <div class="drawer-side">
+      <label for="my-drawer-2" class="drawer-overlay"></label> 
+      <ul class="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
+        {/* <!-- Sidebar content here --> */}
+        <li><Link to="/dashboard" >Add product</Link></li>
+        <li><Link to="/dashboard/manage">Manage product</Link></li>
+      </ul>
+    
+    </div>
+  </div>
+  );
 };
 
 export default Dashboard;
